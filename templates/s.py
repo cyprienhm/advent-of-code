7a090352821ddc7b7ccba0d177ec3840b502badd
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-10-30 08:23:19 -0400

    update template to make script runnable anywhere, add .gitignore for inputs

--- templates/s.py
+++ templates/s.py
@@ -1,21 +1,22 @@
 import sys
 from pathlib import Path
 
-example = Path("example.txt")
-true_input = Path("input.txt")
+example = Path(__file__).parent / "example.txt"
+true_input = Path(__file__).parent / "input.txt"
 to_read = example
 
 if not to_read.exists():
     sys.exit()
 
+print(f"running with {to_read.name}")
 data = open(to_read).read().strip().split("\n")
 
 
-def part1(): ...
+def part1(data: list[str]): ...
 
 
-def part2(): ...
+def part2(data: list[str]): ...
 
 
-print(part1())
-print(part2())
+print(part1(data))
+print(part2(data))
