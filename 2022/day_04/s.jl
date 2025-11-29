c3c2a9ead3967b203f14e50e7c95e6756e3d8914
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-11-28 22:13:38 -0500

    2022/04 (jl): use pipes + vectorized ops

--- 2022/day_04/s.jl
+++ 2022/day_04/s.jl
@@ -3,9 +3,7 @@ function y_contains_x(x, y)
 end
 
 function part1(filename)
-  data = read(filename, String)
-  data = split(strip(data), "\n")
-  data = map(pairs->map(ranges->map(elt->parse(Int, elt), split(ranges,"-")),split(pairs, ",")), data)
+  data = read(filename, String) |> txt -> split(strip(txt), "\n") .|> pairs->map(ranges->map(elt->parse(Int, elt), split(ranges,"-")),split(pairs, ","))
   res = sum(map(x->y_contains_x(x[1],x[2]) || y_contains_x(x[2],x[1]), data))
   return res
 end
@@ -16,13 +14,11 @@ function overlaps(x,y)
 end
 
 function part2(filename)
-  data = read(filename, String)
-  data = split(strip(data), "\n")
-  data = map(pairs->map(ranges->map(elt->parse(Int, elt), split(ranges,"-")),split(pairs, ",")), data)
+  data = read(filename, String) |> (txt -> split(strip(txt), "\n")) .|> line -> split(line,",") .|> (pair -> split(pair, "-") .|> (x->parse(Int,x)))
   res = sum(map(x->overlaps(x[1],x[2]), data))
   return res
 end
 
-filename = "input.txt"
+filename = "example.txt"
 println(part1(filename))
 println(part2(filename))
