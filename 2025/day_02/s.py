c71f172930bd3332dcdd2136a3c6753c9963cb9f
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-12-02 00:29:35 -0500

    2025/02 (py): comprehensions go brrr

--- 2025/day_02/s.py
+++ 2025/day_02/s.py
@@ -10,46 +10,52 @@ data = open(to_read).read().strip()
 
 def invalid(n: int):
     ns = str(n)
-    for i in range(len(ns)):
-        if ns[:i] == ns[i:]:
-            return True
-    return False
+    return any(ns[:i] == ns[i:] for i in range(len(ns)))
 
 
 def invalidp2(n: int):
     ns = str(n)
-    for i in range(1, len(ns)):
-        if len(ns) % i != 0:
-            continue
+    # for i in range(1, len(ns)):
+    #     if len(ns) % i != 0:
+    #         continue
+    #
+    #     amount = len(ns) // i
+    #
+    #     if all(
+    #       ns[:i] == ns[rep * i : (rep + 1) * i] for rep in range(amount)
+    #     ):
+    #         return True
+    # return False
+    return any(
+        len(ns) % i == 0
+        and all(
+            ns[:i] == ns[rep * i : (rep + 1) * i] for rep in range(len(ns) // i)
+        )
+        for i in range(1, len(ns))
+    )
 
-        amount = len(ns) // i
 
-        if all(ns[:i] == ns[rep * i : (rep + 1) * i] for rep in range(amount)):
-            return True
-    return False
-
-
-def part1(data: list[str]):
-    ranges = [c.strip().split("-") for c in data.split(",")]
+def part1(data: str):
+    ranges = [
+        [int(elt) for elt in c.strip().split("-")] for c in data.split(",")
+    ]
 
     l = []
     for start, end in ranges:
-        startn = int(start)
-        endn = int(end)
-        for n in range(startn, endn + 1):
+        for n in range(start, end + 1):
             if invalid(n):
                 l.append(n)
     return sum(l)
 
 
-def part2(data: list[str]):
-    ranges = [c.strip().split("-") for c in data.split(",")]
+def part2(data: str):
+    ranges = [
+        [int(elt) for elt in c.strip().split("-")] for c in data.split(",")
+    ]
 
     l = []
     for start, end in ranges:
-        startn = int(start)
-        endn = int(end)
-        for n in range(startn, endn + 1):
+        for n in range(start, end + 1):
             if invalidp2(n):
                 l.append(n)
     return sum(l)
