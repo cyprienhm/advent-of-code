8f054f4004caf04d59786cec398e6a24b9166a87
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-12-03 18:28:48 -0500

    2025/03 (hs)

---
+++ 2025/day_03/s.hs
@@ -0,0 +1,29 @@
+import Data.Char (digitToInt)
+import Data.List (minimumBy)
+
+maxAndIndex :: [Int] -> (Int, Int)
+maxAndIndex l = minimumBy (\x y -> compare (-snd x) (-snd y)) (zip [0 ..] l)
+
+pickBest :: [Int] -> Int -> [Int]
+pickBest s tot
+  | tot == 0 = []
+  | otherwise = best : pickBest rem (tot - 1)
+  where
+    budget = length s - tot + 1
+    toPickFrom = take budget s
+    (bestIndex, best) = maxAndIndex toPickFrom
+    rem = drop (bestIndex + 1) s
+
+joltage :: [Int] -> Int -> Int
+joltage battery n = foldl (\acc x -> acc * 10 + x) 0 (pickBest battery n)
+
+main :: IO ()
+main = do
+  input <- lines <$> readFile "input.txt"
+  let batteries = map (map digitToInt) input
+
+  let part1 = sum $ map (`joltage` 2) batteries
+  print part1
+
+  let part2 = sum $ map (`joltage` 12) batteries
+  print part2
