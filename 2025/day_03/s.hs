1f4904ea0575d5096ff4e70121a6db4b94545efe
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-12-03 18:34:41 -0500

    2025/03 (hs): pattern match, refactor

--- 2025/day_03/s.hs
+++ 2025/day_03/s.hs
@@ -5,17 +5,19 @@ maxAndIndex :: [Int] -> (Int, Int)
 maxAndIndex l = minimumBy (\x y -> compare (-snd x) (-snd y)) (zip [0 ..] l)
 
 pickBest :: [Int] -> Int -> [Int]
-pickBest s tot
-  | tot == 0 = []
-  | otherwise = best : pickBest rem (tot - 1)
+pickBest s 0 = []
+pickBest s tot = best : pickBest rem (tot - 1)
   where
     budget = length s - tot + 1
     toPickFrom = take budget s
     (bestIndex, best) = maxAndIndex toPickFrom
     rem = drop (bestIndex + 1) s
 
+listToInt :: [Int] -> Int
+listToInt = foldl (\acc x -> acc * 10 + x) 0
+
 joltage :: [Int] -> Int -> Int
-joltage battery n = foldl (\acc x -> acc * 10 + x) 0 (pickBest battery n)
+joltage battery n = listToInt $ pickBest battery n
 
 main :: IO ()
 main = do
