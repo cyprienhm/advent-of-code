c99bad4e9217b4d752ab32e8c6952608817bbfed
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-12-04 00:51:37 -0500

    2025/04 (py): add functional version of rolls positions function

--- 2025/day_04/s.py
+++ 2025/day_04/s.py
@@ -29,12 +29,30 @@ def get_rolls_pos(data: list[list[str]]):
     return res
 
 
+def get_rolls_pos_functional(data: list[list[str]]):
+    dirs = set(product([-1, 0, 1], [-1, 0, 1])) - {(0, 0)}
+
+    positions = list(product(range(len(data)), range(len(data[0]))))
+    rolls = list(filter(lambda pos: data[pos[0]][pos[1]] == "@", positions))
+
+    return list(
+        filter(
+            lambda rowcol: sum(
+                data[rowcol[0] + up][rowcol[1] + right] == "@"
+                for up, right in dirs
+            )
+            < 4,
+            rolls,
+        )
+    )
+
+
 def part1(data: list[list[str]]):
     return len(get_rolls_pos(data))
 
 
 def part2(data: list[list[str]]):
-    to_remove = get_rolls_pos(data)
+    to_remove = get_rolls_pos_functional(data)
     ans = 0
     while len(to_remove) > 0:
         for i, j in to_remove:
