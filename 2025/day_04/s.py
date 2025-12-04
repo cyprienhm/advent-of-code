dac61e1b3ce7f0698fe8f0f38a1a625480bfbe2f
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-12-04 00:15:00 -0500

    2025/04 (py)

---
+++ 2025/day_04/s.py
@@ -0,0 +1,89 @@
+from pathlib import Path
+
+example = Path(__file__).parent / "example.txt"
+true_input = Path(__file__).parent / "input.txt"
+to_read = true_input
+
+print(f"running with {to_read.name}")
+data = open(to_read).read().strip().split("\n")
+
+
+def part1(data: list[str]):
+    dirs = [
+        (-1, 0),
+        (1, 0),
+        (0, -1),
+        (0, 1),
+        (-1, -1),
+        (1, 1),
+        (1, -1),
+        (-1, 1),
+    ]
+    res = 0
+    for row, row_line in enumerate(data):
+        for col, elt in enumerate(row_line):
+            if elt == "@":
+                around = 0
+                for d in dirs:
+                    up = d[0]
+                    right = d[1]
+                    if (
+                        row + up >= 0
+                        and row + up < len(data)
+                        and col + right < len(data[0])
+                        and col + right >= 0
+                    ):
+                        if data[row + up][col + right] == "@":
+                            around += 1
+                if around < 4:
+                    res += 1
+    return res
+
+
+def get_rolls_pos(data: list[str]):
+    dirs = [
+        (-1, 0),
+        (1, 0),
+        (0, -1),
+        (0, 1),
+        (-1, -1),
+        (1, 1),
+        (1, -1),
+        (-1, 1),
+    ]
+    res = []
+    for row, row_line in enumerate(data):
+        for col, elt in enumerate(row_line):
+            if elt == "@":
+                around = 0
+                for d in dirs:
+                    up = d[0]
+                    right = d[1]
+                    if (
+                        row + up >= 0
+                        and row + up < len(data)
+                        and col + right < len(data[0])
+                        and col + right >= 0
+                    ):
+                        if data[row + up][col + right] == "@":
+                            around += 1
+                if around < 4:
+                    res.append((row, col))
+    return res
+
+
+def part2(data: list[str]):
+    to_remove = get_rolls_pos(data)
+
+    data = [[s for s in c] for c in data]
+    ans = 0
+    while len(to_remove) > 0:
+        for i, j in to_remove:
+            data[i][j] = "."
+        ans += len(to_remove)
+        to_remove = get_rolls_pos(data)
+    return ans
+
+
+print(part1(data))
+print(part2(data))
