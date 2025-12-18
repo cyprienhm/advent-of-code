d66dab76399aabdc4cfa09ee039511f03035c05a
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-12-17 20:15:31 -0500

    2022/09 (py)

---
+++ 2022/day_09/s.py
@@ -0,0 +1,121 @@
+import enum
+from pathlib import Path
+
+example = Path(__file__).parent / "example.txt"
+example2 = Path(__file__).parent / "example2.txt"
+true_input = Path(__file__).parent / "input.txt"
+to_read = true_input
+
+print(f"running with {to_read.name}")
+data = open(to_read).read().strip().split("\n")
+
+
+def part1(data: list[str]):
+    instructions = [(c[0], int(c[2:])) for c in data]
+    hx = 0
+    hy = 0
+    tx = 0
+    ty = 0
+
+    pos_visited = set()
+    for direction, amount in instructions:
+        if direction == "R":
+            for _ in range(amount):
+                hx += 1
+                if abs(hx - tx) > 1:
+                    tx += 1
+                    if ty != hy:
+                        ty = hy
+                pos_visited.add((tx, ty))
+        if direction == "L":
+            for _ in range(amount):
+                hx -= 1
+                if abs(hx - tx) > 1:
+                    tx -= 1
+                    if ty != hy:
+                        ty = hy
+                pos_visited.add((tx, ty))
+        if direction == "U":
+            for _ in range(amount):
+                hy += 1
+                if abs(hy - ty) > 1:
+                    ty += 1
+                    if tx != hx:
+                        tx = hx
+                pos_visited.add((tx, ty))
+        if direction == "D":
+            for _ in range(amount):
+                hy -= 1
+                if abs(hy - ty) > 1:
+                    ty -= 1
+                    if tx != hx:
+                        tx = hx
+                pos_visited.add((tx, ty))
+    return len(pos_visited)
+
+
+def join_back(cur: tuple[int, int], prev: tuple[int, int]) -> tuple[int, int]:
+    curx, cury = cur
+    prevx, prevy = prev
+    if abs(cury - prevy) > 1:
+        cury += 1 if cury < prevy else 0 if cury == prevy else -1
+        curx += 1 if curx < prevx else 0 if curx == prevx else -1
+    if abs(curx - prevx) > 1:
+        curx += 1 if curx < prevx else 0 if curx == prevx else -1
+        cury += 1 if cury < prevy else 0 if cury == prevy else -1
+    return (curx, cury)
+
+
+def part2(data: list[str]):
+    instructions: list[tuple[str, int]] = [(c[0], int(c[2:])) for c in data]
+    snake: list[tuple[int, int]] = [(0, 0) for _ in range(10)]
+
+    pos_visited: set[tuple[int, int]] = set()
+    for direction, amount in instructions:
+        if direction == "R":
+            for _ in range(amount):
+                head_x, head_y = snake[0]
+                snake[0] = (head_x + 1, head_y)
+
+                for i in range(1, len(snake)):
+                    snake[i] = join_back(snake[i], snake[i - 1])
+                pos_visited.add(snake[-1])
+        if direction == "L":
+            for _ in range(amount):
+                head_x, head_y = snake[0]
+                snake[0] = (head_x - 1, head_y)
+
+                for i in range(1, len(snake)):
+                    snake[i] = join_back(snake[i], snake[i - 1])
+                pos_visited.add(snake[-1])
+        if direction == "U":
+            for _ in range(amount):
+                head_x, head_y = snake[0]
+                snake[0] = (head_x, head_y + 1)
+
+                for i in range(1, len(snake)):
+                    snake[i] = join_back(snake[i], snake[i - 1])
+                pos_visited.add(snake[-1])
+        if direction == "D":
+            for _ in range(amount):
+                head_x, head_y = snake[0]
+                snake[0] = (head_x, head_y - 1)
+
+                for i in range(1, len(snake)):
+                    snake[i] = join_back(snake[i], snake[i - 1])
+                pos_visited.add(snake[-1])
+    return len(pos_visited)
+
+
+def print_snake(snake: list[tuple[int, int]]):
+    print(snake)
+    to_print = [["." for _ in range(-25, 25)] for _ in range(-10, 10)]
+    for i, (x, y) in enumerate(snake):
+        to_print[-y + 10][x + 25] = str(i)
+    print("==")
+    print("\n".join(["".join(c) for c in to_print]))
+    print("==")
+
+
+print(part1(data))
+print(part2(data))
